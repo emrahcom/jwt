10e86d7a29c3ee684115cb97df6a7ce4c5437516
emrah <emrah.eryilmaz@conceptboard.com>
2025-09-27 14:55:27 +0300

    refactor: import lines in examples

--- examples/pkcs8_storing.ts
+++ examples/pkcs8_storing.ts
@@ -3,7 +3,11 @@
 //   deno run pkcs8_storing.ts
 // -----------------------------------------------------------------------------
 
-import { decodeBase64, encodeBase64 } from "jsr:@std/encoding";
+import { decodeBase64, encodeBase64 } from "@std/encoding";
+
+// If you dont have deno.json and @std/encoding is not in its import list
+// then use these import line:
+// import { decodeBase64, encodeBase64 } from "jsr:@std/encoding@^1.0.10";
 
 /*
   Import a PEM encoded RSA private key, to use for RSA-PSS signing.
